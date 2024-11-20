86c53e35767dfa88d2345acd90c48c089d832b25
niloferh <nilofer.hyder@mail.utoronto.ca>
2024-11-19 21:30:55 -0500

    Addressed comments

--- treatmentserver/views.py
+++ treatmentserver/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 from treatmentserver.models import TreatmentSessions
 def index(request):
@@ -22,14 +23,18 @@ def get_video_call_id(request):
     except TreatmentSessions.DoesNotExist:
         return JsonResponse({"message": "Video Call ID not found"})
 
+@api_view(['GET'])
 def get_session_info(request):
     treatment_id = request.GET.get("id")
     try:
         obj = TreatmentSessions.objects.get(id=treatment_id)
+        if obj is None:
+            return JsonResponse({"message": "Treatment session id not found"}, status=400)
+        
         return JsonResponse({
             "session_number": str(obj.session_number),
             "date": str(obj.date_scheduled),
             "time": str(obj.start_time_scheduled)
         })
-    except TreatmentSessions.DoesNotExist:
-        return JsonResponse({"message": "Treatment session info not found"})
+    except Exception as e:
+        return JsonResponse({'message':str(e)}, status=500)
