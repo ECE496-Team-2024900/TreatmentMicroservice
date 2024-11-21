7125188ef2c30289eab73609dcbca5aa7774f21e
zoya5636 <83867635+zoya5636@users.noreply.github.com>
2024-11-20 20:21:47 -0500

    Delete treatmentserver/serializers.py

--- treatmentserver/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from base.models import TreatmentSession
-
-class TreatmentSessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TreatmentSession
-        fields = '__all__'
